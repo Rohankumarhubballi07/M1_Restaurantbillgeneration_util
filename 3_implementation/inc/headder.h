d55bc71a42209edc66c48296921af2a1bd614265
Rohankumarhubballi07 <rohankumarhubbali@gmail.com>
2022-03-04 16:11:59 +0530

    update

--- 3_implementation/inc/headder.h
+++ 3_implementation/inc/headder.h
@@ -1,9 +1,24 @@
+/**
+ * @file headder.h
+ * @author rohankumar (you@domain.com)
+ * @brief project is about restaurant bill generation c programming code 
+ * @version 0.1
+ * @date 2022-03-04
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
 #ifndef _headder_H_
 #define _headder_H_
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-
+/**
+ * @brief here this function is to fetch details of customer
+ * 
+ */
 struct orders
 {
     char customer[50];
